da41ee737aace5b83fd0954d368f0ff5f82d0b02
Tanner Lewis <lewijacn@amazon.com>
2024-05-22 19:53:22 -0400

    Update gradlew command for MA changes and remove Java export
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- buildCaptureProxy.sh
+++ buildCaptureProxy.sh
@@ -4,7 +4,8 @@
 
 git_http_url=$1
 branch=$2
-export JAVA_HOME=/home/ec2-user/elasticsearch/jdk
+# This must be match Java requirement from Migration Assistant (Java 11 currently)
+#export JAVA_HOME=/home/ec2-user/elasticsearch/jdk
 
 mkdir -p /home/ec2-user/capture-proxy/opensearch-migrations
 cd /home/ec2-user/capture-proxy/opensearch-migrations || exit
@@ -18,24 +19,8 @@ else
   git remote set-url origin "$git_http_url"
 fi
 
-git config core.sparseCheckout true
-# Check file exists and contains sparse-checkout
-if test -f .git/info/sparse-checkout; then
-  sparse_entry=$(cat .git/info/sparse-checkout | grep "/TrafficCapture")
-  if [ -z "${sparse_entry}" ]; then
-    echo "No '/TrafficCapture' entry in '.git/info/sparse-checkout' file detected, will attempt to add"
-    git remote add -f origin "$git_http_url"
-  else
-    echo "Have detected '/TrafficCapture' entry in '.git/info/sparse-checkout' file, no changes needed"
-  fi
-else
-  echo "File '.git/info/sparse-checkout' not found, will attempt to create"
-  echo "/TrafficCapture" >> .git/info/sparse-checkout
-fi
-
 git pull origin "$branch"
-cd TrafficCapture || exit
-./gradlew build -p trafficCaptureProxyServer -x test
+./gradlew TrafficCapture:trafficCaptureProxyServer:build -x test
 
 dist_age_seconds=$(( `date +%s` - `stat -L --format %Y "/home/ec2-user/capture-proxy/opensearch-migrations/TrafficCapture/trafficCaptureProxyServer/build/distributions/trafficCaptureProxyServer.zip"` ))
 echo "Capture Proxy distribution was created $dist_age_seconds seconds ago"
